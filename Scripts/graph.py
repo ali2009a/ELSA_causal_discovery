8d0733726d058c727bbe18c7e8ccafaa6668822e
ali2009a <aliarab2009@gmail.com>
2019-07-28 17:51:37 -0700

    Suppor biased nodes in graph.py

--- Scripts/graph.py
+++ Scripts/graph.py
@@ -2,29 +2,29 @@ import numpy as np
 from tqdm import tqdm
 import re
 def levenshtein(seq1, seq2):
-    size_x = len(seq1) + 1
-    size_y = len(seq2) + 1
-    matrix = np.zeros ((size_x, size_y))
-    for x in xrange(size_x):
-        matrix [x, 0] = x
-    for y in xrange(size_y):
-        matrix [0, y] = y
+	size_x = len(seq1) + 1
+	size_y = len(seq2) + 1
+	matrix = np.zeros ((size_x, size_y))
+	for x in xrange(size_x):
+		matrix [x, 0] = x
+	for y in xrange(size_y):
+		matrix [0, y] = y
 
-    for x in xrange(1, size_x):
-        for y in xrange(1, size_y):
-            if seq1[x-1] == seq2[y-1]:
-                matrix [x,y] = min(
-                    matrix[x-1, y] + 1,
-                    matrix[x-1, y-1],
-                    matrix[x, y-1] + 1
-                )
-            else:
-                matrix [x,y] = min(
-                    matrix[x-1,y] + 1,
-                    matrix[x-1,y-1] + 1,
-                    matrix[x,y-1] + 1
-                )
-    return (matrix[size_x - 1, size_y - 1])
+	for x in xrange(1, size_x):
+		for y in xrange(1, size_y):
+			if seq1[x-1] == seq2[y-1]:
+				matrix [x,y] = min(
+					matrix[x-1, y] + 1,
+					matrix[x-1, y-1],
+					matrix[x, y-1] + 1
+				)
+			else:
+				matrix [x,y] = min(
+					matrix[x-1,y] + 1,
+					matrix[x-1,y-1] + 1,
+					matrix[x,y-1] + 1
+				)
+	return (matrix[size_x - 1, size_y - 1])
 
 patterns = []
 
@@ -32,7 +32,10 @@ alphabests= [0,1,2]
 
 def genSeq(prefix, k):
 	if (k==0):
-		patterns.append(prefix)
+		# print prefix
+		# print  prefix==1?
+		if (np.array(prefix)==1).any():
+			patterns.append(prefix)
 		return
 
 	for i in range(0,len(alphabests)):
@@ -49,67 +52,72 @@ def genSeq(prefix, k):
 
 
 class Node:
-  def __init__(self, pval=np.nan, ACE=np.nan, n= np.nan, isLowEnt = False):
-    self.pval = pval
-    self.ACE = ACE
-    self.n=n    
-    self.isLowEnt = isLowEnt  
+  def __init__(self, pval=np.nan, ACE=np.nan, n= np.nan, isLowEnt = False, isBiased=False):
+	self.pval = pval
+	self.ACE = ACE
+	self.n=n    
+	self.isLowEnt = isLowEnt  
+	self.isBiased= isBiased
 
 def genrateAllNodes(n=4):
-    genSeq([],n)
-    dic={}
-    for idx,pattern in enumerate(patterns):
-        dic[re.sub("[^0-9]", "", str(pattern))]= Node()
-    return dic
+	genSeq([],n)
+	dic={}
+	for idx,pattern in enumerate(patterns):
+		dic[re.sub("[^0-9]", "", str(pattern))]= Node()
+	return dic
 
 def readExecutedLowEntNodes(nodes):
-    with open("result.txt") as f:
-        for line in f:
-            parts = line.split(",")
-            pattern = re.sub("[^0-9]", "", parts[0].split(":")[1])
-            pval =  parts[1].split("=")[1].strip()
-            ACE= parts[2].split("=")[1].strip()
-            n= parts[3].split("=")[1].strip()
-            nodes [ pattern] = Node(pval, ACE, n, True)
-    return nodes
+	with open("../result.txt") as f:
+		for line in f:
+			parts = line.split(",")
+			pattern = re.sub("[^0-9]", "", parts[0].split(":")[1])
+			pval =  parts[1].split("=")[1].strip()
+			ACE= parts[2].split("=")[1].strip()
+			n= parts[3].split("=")[1].strip()
+			if ";" in line:
+				nodes [ pattern] = Node(pval, ACE, n, True, True)
+			else:
+				nodes [ pattern] = Node(pval, ACE, n, True, False)
+	return nodes
 
 def createGraph(nodes):
-    graph={}
-    for i, pattern_i in nodes.iteritems(): 
-        adjList = list()
-        for j, pattern_j in nodes.iteritems():
-            if (levenshtein(i, j)==1):
-                adjList.append(j)
-        graph[i] = adjList
-    return graph
+	graph={}
+	for i, pattern_i in nodes.iteritems(): 
+		adjList = list()
+		for j, pattern_j in nodes.iteritems():
+			if (levenshtein(i, j)==1):
+				adjList.append(j)
+		graph[i] = adjList
+	return graph
 
 def writeGraphtoFile(graph, nodes):
-    with open("graph.gv", "w") as f:
-        f.write("strict graph G {\n")
-        for pattern, node in graph.iteritems():
-            if not (nodes[pattern].isLowEnt):
-                continue
-            if  nodes[pattern].isLowEnt:   
-                f.write('\t{} [color="red",label="{}, pval:{}"];\n'.format(pattern, pattern, nodes[pattern].pval))
-            else:
-                f.write('\t{} [label="{}, pval:{}"];\n'.format(pattern, pattern, nodes[pattern].pval))
-        for pattern, adjList in graph.iteritems(): 
-            for adjPattern in adjList:
-                # print pattern, adjPattern
-                # print 
-                if not (nodes[pattern].isLowEnt) or not  (nodes[adjPattern].isLowEnt) :
-                    continue
-                f.write("\t{} -- {};\n".format(pattern, adjPattern))
-        f.write("}")
+	with open("graph.gv", "w") as f:
+		f.write("strict graph G {\n")
+		for pattern, node in graph.iteritems():
+			# if not (nodes[pattern].isLowEnt) or (nodes[pattern].isBiased):
+			#     continue
+			if  nodes[pattern].isBiased:   
+				f.write('\t{} [color="red",label="{}, pval:{}"];\n'.format(pattern, pattern, nodes[pattern].pval))
+			
+			else:
+				f.write('\t{} [label="{}, pval:{}"];\n'.format(pattern, pattern, nodes[pattern].pval))
+		for pattern, adjList in graph.iteritems(): 
+			for adjPattern in adjList:
+				# print pattern, adjPattern
+				# print 
+				# if not (nodes[pattern].isLowEnt) or not  (nodes[adjPattern].isLowEnt) :
+				#     continue
+				f.write("\t{} -- {};\n".format(pattern, adjPattern))
+		f.write("}")
 
 
 def main():
-    nodes = genrateAllNodes(3)
-    # A  = np.array(patterns)
-    # np.savetxt("LowH_Hyp.csv", A.astype(int), delimiter=",",fmt='%s')
-    nodes = readExecutedLowEntNodes(nodes)
-    graph = createGraph(nodes)
-    writeGraphtoFile(graph, nodes)
+	nodes = genrateAllNodes(3)
+	# A  = np.array(patterns)
+	# np.savetxt("LowH_Hyp.csv", A.astype(int), delimiter=",",fmt='%s')
+	nodes = readExecutedLowEntNodes(nodes)
+	graph = createGraph(nodes)
+	writeGraphtoFile(graph, nodes)
 
 
 
